de6d484adae9926bdedd11fc8b4f2b32dcedcece
Maxim Kocheshkov <gror_godfroy@mail.ru>
2020-04-24 18:07:59 +0300

    версия 4.1(Первый вариант на проверку)

--- user_class.py
+++ user_class.py
@@ -2,13 +2,6 @@ from urllib.parse import urlencode
 import requests
 from pprint import pprint
 
-API = 'https://api.vk.com/method'
-
-params = {
-        'access_token': self.token,
-        'user_ids': self.user_id,
-        'v': 5.103, 
-}
 
 class USER():
     def __init__(self, token, user_id):
@@ -17,22 +10,35 @@ class USER():
 
     def get_user(self):
         user_info = requests.get(
-            'API/users.get',
-            params = params
+            'https://api.vk.com/method/users.get',
+            params = {
+                'access_token': self.token,
+                'user_ids': self.user_id,
+                'v': 5.103, 
+        }
         )
         return user_info.json()
 
     def get_groups(self):
         user_groups = requests.get(
-            'API/groups.get',
-            params = params
+            'https://api.vk.com/method/groups.get',
+            params = {
+                'access_token': self.token,
+                'user_ids': self.user_id,
+                'extended': 1,
+                'v': 5.103, 
+            }
         )
         return user_groups.json()
 
     def get_friends(self):
         user_friends = requests.get(
-            'API/friends.get',
-            params = params
+            'https://api.vk.com/method/friends.get',
+            params = {
+                'access_token': self.token,
+                'user_ids': self.user_id,
+                'v': 5.103, 
+            }
         )
         return user_friends.json()
 
